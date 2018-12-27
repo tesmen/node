8c7a1c5f784a040312e696dcdfcdcf14e49aeb5b
tesmen <tezmo@mail.ru>
2018-12-27 17:13:55 +0300

    app

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
-const db = require('./lib/core/db');
+'use strict';
 
-const app = require('http').createServer(handler)
-    , io = require('socket.io').listen(app)
-    , fs = require('fs');
-
-app.listen(3000);
-
-function handler(req, res) {
-    fs.readFile(__dirname + '/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-
-            res.writeHead(200);
-            res.end(data);
-        });
-}
-
-async function mouseHandler(data) {
-    db.query(`INSERT INTO coordinates (x,y) VALUES (${data.x}, ${data.y})`);
-    console.log(data);
-}
-
-
-io.sockets.on('connection', function (socket) {
-    socket.emit('news', {hello: 'world'});
-    socket.on('mouse', mouseHandler);
-});
+const
+    db = require('./lib/core/db'),
+    cfg = require('./lib/core/cfg'),
+    server = require('./lib/core/server')
+;
